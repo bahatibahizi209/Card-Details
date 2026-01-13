7f82ec30ce0bc56d10a08556624e4cf287a07c4b
bahatibahizi209 <bahatibahizi209@gmail.com>
2026-01-13 12:03:52 +0200

    new codes submitted

--- src/components/CardFront.jsx
+++ src/components/CardFront.jsx
@@ -5,6 +5,25 @@ function CardFront() {
     {/* Decorate Light circle */}
     <div className="absolute top-0 right-0 w-64 h-64 bg-white opacity-10 rounded-full -mr-32 -mt-32"></div>
     <div className="absolute bottom-0 left-0 w-64 h-64 bg-white opacity-10 rounded-full -mr-24 -mt-24"></div>
+    <div className="relative z-10 h-full flex flex-col justify-between">
+    {/* Toplogo */}
+    <div className="flex justify-between items-start">
+    <div className="flex items-center gap-2">
+    <div className="w-14 h-11 bg-gradient-to-br from-yellow-200 to-yellow-400 rounded-lg
+    flex items-center justify-center">
+    <div className="w-10 h-7 bg-gradient-to-br from-yellow-400 to-yellow-600"></div>
+    <div className="w-10 h-10 border-2 border-white rounded-full"></div>
+    </div>
+    </div>
+    </div>
+    {/* card number */}
+    <div>
+      <div className="text-white text-3xl font-mono tracking-widest mb-2">
+        0000 0000 0000 0000
+      </div>
+      <div className="text-white-70 text-xs mb-1 uppercase">Country</div>
+    </div>
+    </div>
     </div>
   )
 }
--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -3,8 +3,10 @@ import CardFront from "./CardFront"
 function CardPreview() {
  return (
     <div className="space-y-8">
+    <div className="relative perspective-1000" style={{minHeight:"280px"}}>
     <div className={`absolute inset-0 transition-all duration-700`}>
     <CardFront/>
+    </div>
     </div>
     </div>
   )
