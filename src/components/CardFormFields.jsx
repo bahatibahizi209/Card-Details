3e54d1bbeeaf1b1eab503bf3d4f1cb5e89d17e35
bahatibahizi209 <bahatibahizi209@gmail.com>
2026-01-16 12:07:36 +0200

    new codes submitted

--- src/components/CardFormFields.jsx
+++ src/components/CardFormFields.jsx
@@ -6,13 +6,13 @@ function CardFormFields() {
     // </div>
     <form className="bg-white rounded-2xl p-10 shadow-2xl">
     <div className="mb-8">
-    <h2 className="text-3xl font-bold text-gray-900 mb2">Payments details</h2>
-    <p className="text-gray-600">Complete your purchase securely</p>
+    <h2 className="text-3xl font-bold text-gray-900 mb-2">Payments details</h2>
+    <p className="text-gray-600 text-xs">Complete your purchase securely</p>
     </div>
     <div className="space-y-6">
     {/* cardholder name */}
      <div>
-        <input type="text"  placeholder="e.g jane Applessed" 
+        <input type="text"  placeholder="e.g paul george" 
         className={`w-full px-4 py-3.5 bg-gray-50 border-2 rounded-lg text-gray-900
         placeholder-gray-400 focus:outline-none transition-all `}/>    
     </div>
@@ -23,30 +23,31 @@ function CardFormFields() {
         placeholder-gray-400 focus:outline-none transition-all `}/>    
     </div>   
      {/* Expiry and CVC */}
-    <div className="flex gap-4">
-    <div className="flex-1">
-    <div className="flex gap-2">
+    {/* Expiry and CVC */}
+<div className="flex gap-4">
+  <div className="flex-1 flex gap-2">
     <input type="text" inputMode="numeric" placeholder="MM" 
-    className={`w-full px-4 py-3.5 bg-gray-50 border-2 rounded-lg text-gray-900
-     placeholder-gray-400 focus:outline-none transition-all `}/>    
+      className={`w-full px-4 py-3.5 bg-gray-50 border-2 rounded-lg text-gray-900
+      placeholder-gray-400 focus:outline-none transition-all `}/>    
     <input type="text" inputMode="numeric" placeholder="YY" 
-    className={`w-full px-4 py-3.5 bg-gray-50 border-2 rounded-lg text-gray-900
-     placeholder-gray-400 focus:outline-none transition-all `}/>        
-    </div>
-    </div> 
-     <div className="flex-1">
-    <div className="flex gap-2">
-    <input type="text" inputMode="numeric" placeholder="eg 123" 
-    className={`w-full px-4 py-3.5 bg-gray-50 border-2 rounded-lg text-gray-900
-     placeholder-gray-400 focus:outline-none transition-all `}/>
-     </div>
-     </div>
-    <button type="submit" className="w-full bg-purple-900 text-white py-4 rounded-lg
-       font-semiboldtext-lg hover:bg-purple-800 transition-all shadow-lg 
-       hover:shadow-xl transform hover:scale-[1.02] active:scale-[0.98] mt-4 hover:cursor-pointer">Confirm</button>
-    </div>
-    </div>
-    </form>
+      className={`w-full px-4 py-3.5 bg-gray-50 border-2 rounded-lg text-gray-900
+      placeholder-gray-400 focus:outline-none transition-all `}/>        
+  </div> 
+  
+  <div className="flex-1">
+    <input type="text" inputMode="numeric" placeholder="e.g. 123" 
+      className={`w-full px-4 py-3.5 bg-gray-50 border-2 rounded-lg text-gray-900
+      placeholder-gray-400 focus:outline-none transition-all `}/>
+  </div>
+  
+  <button type="submit" className="flex-1 bg-purple-900 text-white py-4 rounded-lg
+    font-semibold text-lg hover:bg-purple-800 transition-all shadow-lg 
+    hover:shadow-xl transform hover:scale-[1.02] active:scale-[0.98]">
+    Confirm
+  </button>
+</div>
+</div>
+</form>
   )
 }
 
