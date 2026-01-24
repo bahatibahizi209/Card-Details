c4937b89fa8958bc5e9e64185243697fb7290cc0
bahatibahizi209 <bahatibahizi209@gmail.com>
2026-01-24 13:59:05 +0200

    new codes submitted

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -17,6 +17,13 @@ function CardForm() {
   const [error,setError]=useState({})
   const onchangeField=(field,value)=>{
     setError((prev)=>({...prev,[field]: ""}))
+   if(field === "name"){
+  const v =value.replace(/[^0-9]/g, "");
+  if(v.length <=50){
+    setCardData((prev)=>({...prev,name:v}));
+  }
+  return;
+  }
     if(field === "number"){
       const formated=formatCardNumber(value) 
       if (formated.replace(/\s/g, "").length<=16){
@@ -47,6 +54,8 @@ function CardForm() {
   }
   setCardData((prev)=>({...prev,[field]:value}));
 }
+const OnFocusCvc=()=>setFlipped(true)
+const  OnBlurCvc=()=>setFlipped(false)
 // CARD FORM
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-indigo-900
